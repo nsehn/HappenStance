b2f846d1dc3c0e2c6211189311a4e0d2004575cd
Nisha Sehn <nishasehn@gmail.com>
2013-04-15 08:55:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappenStance::Application.config.secret_token = '7202b9e53db38e2dae9320203d7b4b351d37fb3d476dd05f4e78ef60ada032ac81194184eaa3d09210836227c1c14a69f3f08e785a5d37500e9fff1952aaf047'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HappenStance::Application.config.session_store :cookie_store, key: '_HappenStance_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HappenStance::Application.config.session_store :active_record_store
